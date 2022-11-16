a560f5bcb3dd344469062014c090fd3c289a402f
Parth288 <116058274+Parth288@users.noreply.github.com>
2022-11-16 12:13:26 +0530

    Login screen status code changes

--- src/modules/user/screens/login/login_screen.js
+++ src/modules/user/screens/login/login_screen.js
@@ -36,9 +36,8 @@ const LoginScreen = () => {
 		if (result.statusCode === HTTPStatusCode.OK) {
 			setLoading(false);
 			navigate(UTSRoutes.HOMEROUTE);
-		}else{
+		}else if(result.statusCode === HTTPStatusCode.BAD_REQUEST){
 			setLoading(false);
-			navigate(UTSRoutes.LOGINROUTE);
 		}
 	};
 
