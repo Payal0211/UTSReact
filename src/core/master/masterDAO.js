4d34a660d9b0a9fa688227fe79ec4db5dfdd4281
shekharUTS <shekhar.shashank@uplers.in>
2022-12-14 12:55:57 +0530

    dao masters

--- src/core/master/masterDAO.js
+++ src/core/master/masterDAO.js
@@ -31,21 +31,19 @@ export const masterDAO = {
 			return errorDebug(error, 'masterDAO.getFixedValueRequestDAO');
 		}
 	},
-	/* getGEORequest: async function () {
+	getGEORequestDAO: async function () {
 		try {
 			const geoResult = await MasterAPI.getFixedValueRequest();
-			if (fixedValueResult) {
-				const statusCode = fixedValueResult['statusCode'];
+			if (geoResult) {
+				const statusCode = geoResult['statusCode'];
 				if (statusCode === HTTPStatusCode.OK) {
-					const tempResult = fixedValueResult.responseBody;
+					const tempResult = geoResult.responseBody;
 					return {
 						statusCode: statusCode,
 						responseBody: tempResult.details,
 					};
-				} else if (statusCode === HTTPStatusCode.NOT_FOUND)
-					return fixedValueResult;
-				else if (statusCode === HTTPStatusCode.BAD_REQUEST)
-					return fixedValueResult;
+				} else if (statusCode === HTTPStatusCode.NOT_FOUND) return geoResult;
+				else if (statusCode === HTTPStatusCode.BAD_REQUEST) return geoResult;
 				else if (statusCode === HTTPStatusCode.UNAUTHORIZED) {
 					let deletedResponse =
 						UserSessionManagementController.deleteAllSession();
@@ -53,7 +51,182 @@ export const masterDAO = {
 				}
 			}
 		} catch (error) {
-			return errorDebug(error, 'masterDAO.getGEORequest');
+			return errorDebug(error, 'masterDAO.getGEORequestDAO');
 		}
-	}, */
+	},
+	getSkillsRequestDAO: async function () {
+		try {
+			const skillsResult = await MasterAPI.getSkillsRequest();
+			if (skillsResult) {
+				const statusCode = skillsResult['statusCode'];
+				if (statusCode === HTTPStatusCode.OK) {
+					const tempResult = skillsResult.responseBody;
+					return {
+						statusCode: statusCode,
+						responseBody: tempResult.details,
+					};
+				} else if (statusCode === HTTPStatusCode.NOT_FOUND) return skillsResult;
+				else if (statusCode === HTTPStatusCode.BAD_REQUEST) return skillsResult;
+				else if (statusCode === HTTPStatusCode.UNAUTHORIZED) {
+					let deletedResponse =
+						UserSessionManagementController.deleteAllSession();
+					if (deletedResponse) Navigate(UTSRoutes.LOGINROUTE);
+				}
+			}
+		} catch (error) {
+			return errorDebug(error, 'MasterDAO.getSkillsRequestDAO');
+		}
+	},
+	getCurrencyRequestDAO: async function () {
+		try {
+			const currencyResult = await MasterAPI.getCurrencyRequest();
+			if (currencyResult) {
+				const statusCode = currencyResult['statusCode'];
+				if (statusCode === HTTPStatusCode.OK) {
+					const tempResult = currencyResult.responseBody;
+					return {
+						statusCode: statusCode,
+						responseBody: tempResult.details,
+					};
+				} else if (statusCode === HTTPStatusCode.NOT_FOUND)
+					return currencyResult;
+				else if (statusCode === HTTPStatusCode.BAD_REQUEST)
+					return currencyResult;
+				else if (statusCode === HTTPStatusCode.UNAUTHORIZED) {
+					let deletedResponse =
+						UserSessionManagementController.deleteAllSession();
+					if (deletedResponse) Navigate(UTSRoutes.LOGINROUTE);
+				}
+			}
+		} catch (error) {
+			return errorDebug(error, 'MasterDAO.getCurrencyRequestDAO');
+		}
+	},
+	getTalentTimeZoneRequestDAO: async function () {
+		try {
+			const talentTimeZoneResult = await MasterAPI.getTalentTimeZoneRequest();
+			if (talentTimeZoneResult) {
+				const statusCode = talentTimeZoneResult['statusCode'];
+				if (statusCode === HTTPStatusCode.OK) {
+					const tempResult = talentTimeZoneResult.responseBody;
+					return {
+						statusCode: statusCode,
+						responseBody: tempResult.details,
+					};
+				} else if (statusCode === HTTPStatusCode.NOT_FOUND)
+					return talentTimeZoneResult;
+				else if (statusCode === HTTPStatusCode.BAD_REQUEST)
+					return talentTimeZoneResult;
+				else if (statusCode === HTTPStatusCode.UNAUTHORIZED) {
+					let deletedResponse =
+						UserSessionManagementController.deleteAllSession();
+					if (deletedResponse) Navigate(UTSRoutes.LOGINROUTE);
+				}
+			}
+		} catch (error) {
+			return errorDebug(error, 'MasterDAO.getTalentTimeZoneRequestDAO');
+		}
+	},
+	getHowSoonRequestDAO: async function () {
+		try {
+			const howSoonResult = await MasterAPI.getHowSoonRequest();
+			if (howSoonResult) {
+				const statusCode = howSoonResult['statusCode'];
+				if (statusCode === HTTPStatusCode.OK) {
+					const tempResult = howSoonResult.responseBody;
+					return {
+						statusCode: statusCode,
+						responseBody: tempResult.details,
+					};
+				} else if (statusCode === HTTPStatusCode.NOT_FOUND)
+					return howSoonResult;
+				else if (statusCode === HTTPStatusCode.BAD_REQUEST)
+					return howSoonResult;
+				else if (statusCode === HTTPStatusCode.UNAUTHORIZED) {
+					let deletedResponse =
+						UserSessionManagementController.deleteAllSession();
+					if (deletedResponse) Navigate(UTSRoutes.LOGINROUTE);
+				}
+			}
+		} catch (error) {
+			return errorDebug(error, 'MasterDAO.getHowSoonRequestDAO');
+		}
+	},
+	getTimeZonePreferenceRequestDAO: async function () {
+		try {
+			const timeZonePreferenceResult =
+				await MasterAPI.getTimeZonePreferenceRequest();
+			if (timeZonePreferenceResult) {
+				const statusCode = timeZonePreferenceResult['statusCode'];
+				if (statusCode === HTTPStatusCode.OK) {
+					const tempResult = timeZonePreferenceResult.responseBody;
+					return {
+						statusCode: statusCode,
+						responseBody: tempResult.details,
+					};
+				} else if (statusCode === HTTPStatusCode.NOT_FOUND)
+					return timeZonePreferenceResult;
+				else if (statusCode === HTTPStatusCode.BAD_REQUEST)
+					return timeZonePreferenceResult;
+				else if (statusCode === HTTPStatusCode.UNAUTHORIZED) {
+					let deletedResponse =
+						UserSessionManagementController.deleteAllSession();
+					if (deletedResponse) Navigate(UTSRoutes.LOGINROUTE);
+				}
+			}
+		} catch (error) {
+			return errorDebug(error, 'MasterDAO.getTimeZonePreferenceRequestDAO');
+		}
+	},
+	getPartialEngagementTypeRequestDAO: async function () {
+		try {
+			const partialEngagementTypeResult =
+				await MasterAPI.getPartialEngagementTypeRequest();
+			if (partialEngagementTypeResult) {
+				const statusCode = partialEngagementTypeResult['statusCode'];
+				if (statusCode === HTTPStatusCode.OK) {
+					const tempResult = partialEngagementTypeResult.responseBody;
+					return {
+						statusCode: statusCode,
+						responseBody: tempResult.details,
+					};
+				} else if (statusCode === HTTPStatusCode.NOT_FOUND)
+					return partialEngagementTypeResult;
+				else if (statusCode === HTTPStatusCode.BAD_REQUEST)
+					return partialEngagementTypeResult;
+				else if (statusCode === HTTPStatusCode.UNAUTHORIZED) {
+					let deletedResponse =
+						UserSessionManagementController.deleteAllSession();
+					if (deletedResponse) Navigate(UTSRoutes.LOGINROUTE);
+				}
+			}
+		} catch (error) {
+			return errorDebug(error, 'MasterDAO.getPartialEngagementTypeRequestDAO');
+		}
+	},
+	getTalentsRoleRequestDAO: async function () {
+		try {
+			const talentRoleResult = await MasterAPI.getTalentsRoleRequest();
+			if (talentRoleResult) {
+				const statusCode = talentRoleResult['statusCode'];
+				if (statusCode === HTTPStatusCode.OK) {
+					const tempResult = talentRoleResult.responseBody;
+					return {
+						statusCode: statusCode,
+						responseBody: tempResult.details,
+					};
+				} else if (statusCode === HTTPStatusCode.NOT_FOUND)
+					return talentRoleResult;
+				else if (statusCode === HTTPStatusCode.BAD_REQUEST)
+					return talentRoleResult;
+				else if (statusCode === HTTPStatusCode.UNAUTHORIZED) {
+					let deletedResponse =
+						UserSessionManagementController.deleteAllSession();
+					if (deletedResponse) Navigate(UTSRoutes.LOGINROUTE);
+				}
+			}
+		} catch (error) {
+			return errorDebug(error, 'MasterDAO.getTalentsRoleRequestDAO');
+		}
+	},
 };
