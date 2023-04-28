aeb6b329ac8d4f2e7e1a3051a449563c8c7c0e3b
shekharUTS <shekhar.shashank@uplers.in>
2023-04-28 18:19:10 +0530

    acceptHR Done API integration

--- src/modules/hiring request/components/acceptHR/acceptHR.js
+++ src/modules/hiring request/components/acceptHR/acceptHR.js
@@ -5,6 +5,9 @@ import { useCallback, useState } from 'react';
 import HRInputField from '../hrInputFields/hrInputFields';
 import { useForm } from 'react-hook-form';
 import { InputType } from 'constants/application';
+import { hiringRequestDAO } from 'core/hiringRequest/hiringRequestDAO';
+import { HTTPStatusCode } from 'constants/network';
+import { useLocation } from 'react-router-dom';
 const AcceptHR = ({ hrID, openModal, cancelModal }) => {
 	const [showMoreInfo, setMoreInfo] = useState(false);
 	const {
@@ -17,10 +20,45 @@ const AcceptHR = ({ hrID, openModal, cancelModal }) => {
 		watch,
 		formState: { errors },
 	} = useForm({});
-	const toggleHandler = useCallback(() => {
-		setMoreInfo(true);
-	}, []);
+	const switchLocation = useLocation();
 
+	let urlSplitter = `${switchLocation.pathname.split('/')[2]}`;
+	const acceptHRHandler = useCallback(
+		async (d) => {
+			let acceptHRObject = {
+				HRID: urlSplitter,
+				AcceptValue: '1',
+				Reason: d.acceptHRDetails,
+			};
+
+			const response = await hiringRequestDAO.acceptHRRequestDAO(
+				acceptHRObject,
+			);
+			if (response?.statusCode === HTTPStatusCode.OK) {
+				cancelModal();
+				window.location.reload(false);
+			}
+		},
+		[cancelModal, urlSplitter],
+	);
+	const waitForMoreInfoHandler = useCallback(
+		async (d) => {
+			let acceptHRObject = {
+				HRID: urlSplitter,
+				AcceptValue: '0',
+				Reason: d.acceptHRDetails,
+			};
+
+			const response = await hiringRequestDAO.acceptHRRequestDAO(
+				acceptHRObject,
+			);
+			if (response?.statusCode === HTTPStatusCode.OK) {
+				cancelModal();
+				window.location.reload(false);
+			}
+		},
+		[cancelModal, urlSplitter],
+	);
 	return (
 		<Modal
 			width="864px"
@@ -40,28 +78,26 @@ const AcceptHR = ({ hrID, openModal, cancelModal }) => {
 						If you need more clarity on this HR, then change the Status to
 						“Waiting for more information”.
 					</p>
-					{showMoreInfo && (
-						<div className={AcceptHRStyle.colMd12}>
-							<HRInputField
-								required
-								isTextArea={true}
-								register={register}
-								errors={errors}
-								validationSchema={{
-									required: 'please enter the details.',
-								}}
-								label="Add Details which are missing to have more clarity "
-								name={'acceptHRDetails'}
-								type={InputType.TEXT}
-								placeholder="Add Details which are missing or needs more clarity"
-							/>
-						</div>
-					)}
+					<div className={AcceptHRStyle.colMd12}>
+						<HRInputField
+							required
+							isTextArea={true}
+							register={register}
+							errors={errors}
+							validationSchema={{
+								required: 'please enter the details.',
+							}}
+							label="Add Details which are missing to have more clarity "
+							name={'acceptHRDetails'}
+							type={InputType.TEXT}
+							placeholder="Add Details which are missing or needs more clarity"
+						/>
+					</div>
 
 					<div className={AcceptHRStyle.formPanelAction}>
 						<button
 							// disabled={type === SubmitType.SAVE_AS_DRAFT}
-							// onClick={cancelModal}
+							onClick={handleSubmit(acceptHRHandler)}
 							className={AcceptHRStyle.btn}>
 							Accept HR
 						</button>
@@ -69,7 +105,7 @@ const AcceptHR = ({ hrID, openModal, cancelModal }) => {
 							<button
 								// disabled={isLoading}
 								type="submit"
-								onClick={toggleHandler}
+								onClick={handleSubmit(waitForMoreInfoHandler)}
 								className={AcceptHRStyle.btnPrimary}>
 								Wait for more Information
 							</button>
