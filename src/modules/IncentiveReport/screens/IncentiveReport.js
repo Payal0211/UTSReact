fd1c916a4033b39fc18448530051d175e0c5c9b1
sundaramUTS <sundaram.n@uplers.in>
2023-05-04 13:13:05 +0530

    Design in incentive report page

--- src/modules/IncentiveReport/screens/IncentiveReport.js
+++ src/modules/IncentiveReport/screens/IncentiveReport.js
@@ -5,7 +5,8 @@ import { ReactComponent as FunnelSVG } from 'assets/svg/funnel.svg';
 import { ReactComponent as SearchSVG } from 'assets/svg/search.svg';
 import { ReactComponent as LockSVG } from 'assets/svg/lock.svg';
 import { ReactComponent as UnlockSVG } from 'assets/svg/unlock.svg';
-import { Dropdown, Menu, message, Modal, Table, Tooltip } from 'antd';
+import { Dropdown, Menu, message, Modal, Table, Tooltip, Select, Switch, Tree } from 'antd';
+import { CarryOutOutlined, CheckOutlined, FormOutlined } from '@ant-design/icons';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useForm } from 'react-hook-form';
@@ -75,6 +76,132 @@ const IncentiveReportScreen = () => {
     const [getManagerEdit, setManagerEdit] = useState([{ id: 0, value: "Select" }])
     const [getUserRoleValue, setUserRoleValue] = useState("Select")
     const [getManagerValue, setManagerValue] = useState("Select")
+
+
+
+
+    const [showLine, setShowLine] = useState(true);
+    const [showIcon, setShowIcon] = useState(false);
+    const [showLeafIcon, setShowLeafIcon] = useState(true);
+
+
+
+
+
+
+
+
+    const treeData = [
+        {
+            title: 'parent 1',
+            key: '0-0',
+            icon: <CarryOutOutlined />,
+            children: [
+                {
+                    title: 'parent 1-0',
+                    key: '0-0-0',
+                    icon: <CarryOutOutlined />,
+                    children: [
+                        {
+                            title: 'leaf',
+                            key: '0-0-0-0',
+                            icon: <CarryOutOutlined />,
+                        },
+                        {
+                            title: (
+                                <>
+                                    <div>multiple line title</div>
+                                    <div>multiple line title</div>
+                                </>
+                            ),
+                            key: '0-0-0-1',
+                            icon: <CarryOutOutlined />,
+                        },
+                        {
+                            title: 'leaf',
+                            key: '0-0-0-2',
+                            icon: <CarryOutOutlined />,
+                        },
+                    ],
+                },
+                {
+                    title: 'parent 1-1',
+                    key: '0-0-1',
+                    icon: <CarryOutOutlined />,
+                    children: [
+                        {
+                            title: 'leaf',
+                            key: '0-0-1-0',
+                            icon: <CarryOutOutlined />,
+                        },
+                    ],
+                },
+                {
+                    title: 'parent 1-2',
+                    key: '0-0-2',
+                    icon: <CarryOutOutlined />,
+                    children: [
+                        {
+                            title: 'leaf',
+                            key: '0-0-2-0',
+                            icon: <CarryOutOutlined />,
+                        },
+                        {
+                            title: 'leaf',
+                            key: '0-0-2-1',
+                            icon: <CarryOutOutlined />,
+                            switcherIcon: <FormOutlined />,
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            title: 'parent 2',
+            key: '0-1',
+            icon: <CarryOutOutlined />,
+            children: [
+                {
+                    title: 'parent 2-0',
+                    key: '0-1-0',
+                    icon: <CarryOutOutlined />,
+                    children: [
+                        {
+                            title: 'leaf',
+                            key: '0-1-0-0',
+                            icon: <CarryOutOutlined />,
+                        },
+                        {
+                            title: 'leaf',
+                            key: '0-1-0-1',
+                            icon: <CarryOutOutlined />,
+                        },
+                    ],
+                },
+            ],
+        },
+    ];
+
+
+
+    const onSelect = (selectedKeys, info) => {
+        console.log('selected', selectedKeys, info);
+    };
+
+    const handleLeafIconChange = (value) => {
+        if (value === 'custom') {
+            return setShowLeafIcon(<CheckOutlined />);
+        }
+        if (value === 'true') {
+            return setShowLeafIcon(true);
+        }
+        return setShowLeafIcon(false);
+    };
+
+
+
+
+
 
 
     const {
@@ -288,6 +415,27 @@ const IncentiveReportScreen = () => {
 			 * ------------ Table Starts-----------
 			 * @Table Part
 			 */}
+
+            <div className={IncentiveReportStyle.tree_custom}>
+                <div>
+                    <Tree
+                        showLine={
+                            showLine
+                                ? {
+                                    showLeafIcon,
+                                }
+                                : false
+                        }
+                        showIcon={showIcon}
+                        defaultExpandedKeys={['0-0-0']}
+                        onSelect={onSelect}
+                        treeData={treeData}
+                    />
+                </div>
+
+            </div>
+
+
             <div className={IncentiveReportStyle.tableDetails}>
                 {isLoading ? (
                     <TableSkeleton />
