d796751bed99f169b4ecfa66114dbe488c147b57
Pawan Tiwari <pawan.t@uplers.in>
2024-04-16 18:15:37 +0530

    close log changes

--- src/modules/hiring request/components/editHRfields/editHRFields.js
+++ src/modules/hiring request/components/editHRfields/editHRFields.js
@@ -1049,14 +1049,15 @@ const EditHRFields = ({
         hrFormDetails.IsProfileView = false
         hrFormDetails.IsVettedProfile = false
       }
-
-      if(watch('fromTime').value === watch('endTime').value){
+      if(type !== SubmitType.SAVE_AS_DRAFT){
+      if(watch('fromTime')?.value === watch('endTime')?.value){
         setIsSavedLoading(false);
         return setError("fromTime", {
           type: "validate",
           message: "Start & End Time is same.",
         });
-      }     
+      } 
+    }    
 
       if (type === SubmitType.SAVE_AS_DRAFT) {        
         if (_isNull(watch("clientName"))) {
