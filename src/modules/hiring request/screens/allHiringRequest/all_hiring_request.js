0b4d1d8f19f43ee13f51bc952048b91fd3bf626f
nisar-uplers <nisar.s@uplers.in>
2024-05-21 17:53:08 +0530

    Loader issue fixed

--- src/modules/hiring request/screens/allHiringRequest/all_hiring_request.js
+++ src/modules/hiring request/screens/allHiringRequest/all_hiring_request.js
@@ -491,7 +491,7 @@ const AllHiringRequestScreen = () => {
 
   return (
     <div className={allHRStyles.hiringRequestContainer}>
-      <WithLoader className="pageMainLoader" showLoader={debouncedSearch?.length?false:isLoading}>
+      {/* <WithLoader className="pageMainLoader" showLoader={debouncedSearch?.length?false:isLoading}> */}
       {contextHolder}
       <div className={allHRStyles.addnewHR}>
         <div className={allHRStyles.hiringRequest}>All Hiring Requests</div>
@@ -910,7 +910,7 @@ const AllHiringRequestScreen = () => {
           />
         </Modal>
       )}
-    </WithLoader>
+    {/* </WithLoader> */}
     </div>
   );
 };
