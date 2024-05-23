ae660032ce230dd6fa7e038f18b24371457b82cb
Pawan Tiwari <pawan.t@uplers.in>
2024-05-23 11:07:54 +0530

    POC section

--- src/modules/company/screens/addCompany/addCompany.js
+++ src/modules/company/screens/addCompany/addCompany.js
@@ -50,7 +50,40 @@ function AddCompany() {
       <ClientSection register ={register} errors={errors} setValue={setValue} watch={watch} /> 
 
     <EngagementSection register ={register} errors={errors} setValue={setValue} watch={watch} />
+
+    <div className={AddNewClientStyle.tabsFormItem}>
+    <div className={AddNewClientStyle.tabsFormItemInner}>
+      <div className={AddNewClientStyle.tabsLeftPanel}>
+        <h3>Add POCs</h3>
+        <p>Please provide the necessary details.</p>
+      </div>
+      <div className={AddNewClientStyle.tabsRightPanel}>
+        <div className={AddNewClientStyle.row}>
+        <div className={AddNewClientStyle.colMd12}>
+        <HRSelectField
+              setValue={setValue}
+              mode={"id/value"}
+              register={register}
+              name="uplersPOCname"
+              label="Uplers's POC name"
+              defaultValue="Enter POC name"
+              options={['1-10 emp','11-50 emp','51-200 emp'].map(item =>({
+                id: item,
+                value: item,
+            }))}
+            required
+            isError={
+              errors["uplersPOCname"] && errors["uplersPOCname"]
+            }
+            errorMsg="Please select POC name."
+            />
+        </div>
+        </div>
        
+      </div>
+      
+    </div>
+    </div>
     </div>
   );
 }
