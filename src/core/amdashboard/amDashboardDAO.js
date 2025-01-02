835e98c051993e31cd8aba5246d1fb7dc0bc5fa4
Pawan Tiwari <pawan.t@uplers.in>
2025-01-02 17:04:53 +0530

    Leave Tab changes

--- src/core/amdashboard/amDashboardDAO.js
+++ src/core/amdashboard/amDashboardDAO.js
@@ -194,5 +194,112 @@ export const amDashboardDAO  = {
             return errorDebug(error, 'amDashboardDAO.getZohoTicketsDAO');
         }
     },
-
+    getTalentLeaveRequestDAO: async (id) => {
+        try {            
+            const allFiltersResult = await amDashboardAPI.getTalentLeaveRequest(id) ;
+            if (allFiltersResult) {
+				const statusCode = allFiltersResult['statusCode'];
+				if (statusCode === HTTPStatusCode.OK) {
+					const tempResult = allFiltersResult.responseBody;
+					return {
+						statusCode: statusCode,
+						responseBody: tempResult.details,
+					};
+				} else if (
+					statusCode === HTTPStatusCode.NOT_FOUND ||
+					statusCode === HTTPStatusCode.INTERNAL_SERVER_ERROR
+				)
+					return allFiltersResult;
+				else if (statusCode === HTTPStatusCode.BAD_REQUEST)
+					return allFiltersResult;
+				else if (statusCode === HTTPStatusCode.UNAUTHORIZED) {
+					UserSessionManagementController.deleteAllSession();
+					window.location.replace(UTSRoutes.LOGINROUTE);
+				}
+			}
+        } catch (error) {
+            return errorDebug(error, 'amDashboardDAO.getTalentLeaveRequestDAO');
+        }
+    },
+    getCalenderLeaveRequestDAO: async (payload) => {
+        try {            
+            const allFiltersResult = await amDashboardAPI.getCalenderLeaveRequest(payload) ;
+            if (allFiltersResult) {
+				const statusCode = allFiltersResult['statusCode'];
+				if (statusCode === HTTPStatusCode.OK) {
+					const tempResult = allFiltersResult.responseBody;
+					return {
+						statusCode: statusCode,
+						responseBody: tempResult.details,
+					};
+				} else if (
+					statusCode === HTTPStatusCode.NOT_FOUND ||
+					statusCode === HTTPStatusCode.INTERNAL_SERVER_ERROR
+				)
+					return allFiltersResult;
+				else if (statusCode === HTTPStatusCode.BAD_REQUEST)
+					return allFiltersResult;
+				else if (statusCode === HTTPStatusCode.UNAUTHORIZED) {
+					UserSessionManagementController.deleteAllSession();
+					window.location.replace(UTSRoutes.LOGINROUTE);
+				}
+			}
+        } catch (error) {
+            return errorDebug(error, 'amDashboardDAO.getCalenderLeaveRequestDAO');
+        }
+    },
+    updateLeaveRequestDAO: async (payload) => {
+        try {            
+            const allFiltersResult = await amDashboardAPI.updateLeaveRequest(payload) ;
+            if (allFiltersResult) {
+				const statusCode = allFiltersResult['statusCode'];
+				if (statusCode === HTTPStatusCode.OK) {
+					const tempResult = allFiltersResult.responseBody;
+					return {
+						statusCode: statusCode,
+						responseBody: tempResult.details,
+					};
+				} else if (
+					statusCode === HTTPStatusCode.NOT_FOUND ||
+					statusCode === HTTPStatusCode.INTERNAL_SERVER_ERROR
+				)
+					return allFiltersResult;
+				else if (statusCode === HTTPStatusCode.BAD_REQUEST)
+					return allFiltersResult;
+				else if (statusCode === HTTPStatusCode.UNAUTHORIZED) {
+					UserSessionManagementController.deleteAllSession();
+					window.location.replace(UTSRoutes.LOGINROUTE);
+				}
+			}
+        } catch (error) {
+            return errorDebug(error, 'amDashboardDAO.updateLeaveRequestDAO');
+        }
+    },
+	approveRejectLeaveDAO: async (payload) => {
+        try {            
+            const allFiltersResult = await amDashboardAPI.approveRejectLeaveRequest(payload) ;
+            if (allFiltersResult) {
+				const statusCode = allFiltersResult['statusCode'];
+				if (statusCode === HTTPStatusCode.OK) {
+					const tempResult = allFiltersResult.responseBody;
+					return {
+						statusCode: statusCode,
+						responseBody: tempResult.details,
+					};
+				} else if (
+					statusCode === HTTPStatusCode.NOT_FOUND ||
+					statusCode === HTTPStatusCode.INTERNAL_SERVER_ERROR
+				)
+					return allFiltersResult;
+				else if (statusCode === HTTPStatusCode.BAD_REQUEST)
+					return allFiltersResult;
+				else if (statusCode === HTTPStatusCode.UNAUTHORIZED) {
+					UserSessionManagementController.deleteAllSession();
+					window.location.replace(UTSRoutes.LOGINROUTE);
+				}
+			}
+        } catch (error) {
+            return errorDebug(error, 'amDashboardDAO.approveRejectLeaveDAO');
+        }
+    },
 }
