53492487d7df9fcc89a37fc9999d470941777836
Pawan Tiwari <pawan.t@uplers.in>
2025-02-14 10:44:34 +0530

    hide company detail message , engagement type based on showPartTime

--- src/modules/hiring request/components/editHRfields/editHRFields.js
+++ src/modules/hiring request/components/editHRfields/editHRFields.js
@@ -1319,9 +1319,9 @@ const EditHRFields = ({
 
     const result = await allCompanyRequestDAO.updateCompanyConfidentialDAO(payload)
 
-    if(result.statusCode === 200){
-      message.success('Successfully Updated Company profile details')
-    }
+    // if(result.statusCode === 200){
+    //   message.success('Successfully Updated Company profile details')
+    // }
   },[confidentialInfo,getHRdetails?.clientDetails_Result]) 
 
   const hrSubmitHandler = useCallback(
@@ -3342,7 +3342,7 @@ const EditHRFields = ({
                               // label={"Hiring Pricing Type"}
                               label={"Employment Type"}
                               defaultValue="Select Hiring Pricing"
-                              options={hrPricingTypes &&  watch('availability')?.value === 'Part Time' ? hrPricingTypes.map((item) => ({ id: item.id, value: item.type })).filter(i=> i.id !== 3) : hrPricingTypes.map((item) => ({ id: item.id, value: item.type }))}
+                              options={hrPricingTypes &&  watch('availability')?.value === 'Part Time' ? hrPricingTypes.map((item) => ({ id: item.id, value: item.type,showPartTime:item.showPartTime })).filter(i=> (i.id !== 3 && i.showPartTime === true )) : hrPricingTypes.map((item) => ({ id: item.id, value: item.type }))}
                               name="hiringPricingType"
                               isError={
                                 errors["hiringPricingType"] &&
