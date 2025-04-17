41732b6e8d4c2c5d8eb4657e910e69640562f9e8
Pawan Tiwari <pawan.t@uplers.in>
2025-04-17 19:40:39 +0530

    add head field on Add popup

--- src/core/taDashboard/taDashboardDRO.js
+++ src/core/taDashboard/taDashboardDRO.js
@@ -144,4 +144,27 @@ export const TaDashboardDAO = {
             return errorDebug(error, 'TaDashboardDAO.getHRTalentDetailsRequestDAO');
         }
     },
+    getGoalsDetailsRequestDAO:async function (pl) {
+        try {
+            const taResult = await TaDashboardAPI.getGoalsDetailsRequest(pl);
+            if (taResult) {
+                const statusCode = taResult['statusCode'];
+                if (statusCode === HTTPStatusCode.OK) {
+                    const tempResult = taResult.responseBody;
+                    return {
+                        statusCode: statusCode,
+                        responseBody: tempResult.details,
+                    };
+                } else if (statusCode === HTTPStatusCode.NOT_FOUND) return taResult;
+                else if (statusCode === HTTPStatusCode.BAD_REQUEST) return taResult;
+                else if (statusCode === HTTPStatusCode.UNAUTHORIZED) {
+                    let deletedResponse =
+                        UserSessionManagementController.deleteAllSession();
+                    if (deletedResponse) window.location.replace(UTSRoutes.LOGINROUTE);
+                }
+            }
+        } catch (error) {
+            return errorDebug(error, 'TaDashboardDAO.getGoalsDetailsRequestDAO');
+        }
+    },
 }
