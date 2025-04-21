9856dea1d267df1483901378b5bd84f70741d1ad
Pawan Tiwari <pawan.t@uplers.in>
2025-04-21 18:50:24 +0530

    add icon change , edit remove ACTION

--- src/core/taDashboard/taDashboardDRO.js
+++ src/core/taDashboard/taDashboardDRO.js
@@ -236,4 +236,27 @@ export const TaDashboardDAO = {
             return errorDebug(error, 'TaDashboardDAO.getALLCommentsDAO');
         }
     },
+    removeTaskDAO:async function (id) {
+        try {
+            const taResult = await TaDashboardAPI.removeTasksRequest(id);
+            if (taResult) {
+                const statusCode = taResult['statusCode'];
+                if (statusCode === HTTPStatusCode.OK) {
+                    const tempResult = taResult.responseBody;
+                    return {
+                        statusCode: statusCode,
+                        responseBody: tempResult.details,
+                    };
+                } else if (statusCode === HTTPStatusCode.NOT_FOUND) return taResult;
+                else if (statusCode === HTTPStatusCode.BAD_REQUEST) return taResult;
+                else if (statusCode === HTTPStatusCode.UNAUTHORIZED) {
+                    let deletedResponse =
+                        UserSessionManagementController.deleteAllSession();
+                    if (deletedResponse) window.location.replace(UTSRoutes.LOGINROUTE);
+                }
+            }
+        } catch (error) {
+            return errorDebug(error, 'TaDashboardDAO.removeTaskDAO');
+        }
+    },
 }
