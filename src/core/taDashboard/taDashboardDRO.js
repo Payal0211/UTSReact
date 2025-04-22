8fc78fec5b176d84b3d40eca5f90c284858fdd51
Pawan Tiwari <pawan.t@uplers.in>
2025-04-22 13:44:39 +0530

    Fastrack changes

--- src/core/taDashboard/taDashboardDRO.js
+++ src/core/taDashboard/taDashboardDRO.js
@@ -259,4 +259,27 @@ export const TaDashboardDAO = {
             return errorDebug(error, 'TaDashboardDAO.removeTaskDAO');
         }
     },
+    insertProfileShearedTargetDAO:async function (pl) {
+        try {
+            const taResult = await TaDashboardAPI.insertProfileShearedTargetRequest(pl);
+            if (taResult) {
+                const statusCode = taResult['statusCode'];
+                if (statusCode === HTTPStatusCode.OK) {
+                    const tempResult = taResult.responseBody;
+                    return {
+                        statusCode: statusCode,
+                        responseBody: tempResult.details,
+                    };
+                } else if (statusCode === HTTPStatusCode.NOT_FOUND) return taResult;
+                else if (statusCode === HTTPStatusCode.BAD_REQUEST) return taResult;
+                else if (statusCode === HTTPStatusCode.UNAUTHORIZED) {
+                    let deletedResponse =
+                        UserSessionManagementController.deleteAllSession();
+                    if (deletedResponse) window.location.replace(UTSRoutes.LOGINROUTE);
+                }
+            }
+        } catch (error) {
+            return errorDebug(error, 'TaDashboardDAO.insertProfileShearedTargetDAO');
+        }
+    },
 }
