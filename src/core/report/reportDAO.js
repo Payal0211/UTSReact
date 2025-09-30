6e0b66f46cd3111954795e340e870d6ccd71002a
Pawan Tiwari <pawan.t@uplers.in>
2025-09-30 19:01:09 +0530

    new Revenue summary

--- src/core/report/reportDAO.js
+++ src/core/report/reportDAO.js
@@ -1799,4 +1799,27 @@ export const ReportDAO = {
 			return errorDebug(error, 'TaDashboardDAO.getAllPOCDashboardDAO');
 		}
 	},
+	getAllPODRevenueDAO:async function (payload) {
+		try {
+			const taResult = await ReportAPI.getAllPODRevenueRequest(payload);
+			if (taResult) {
+				const statusCode = taResult['statusCode'];
+				if (statusCode === HTTPStatusCode.OK) {
+					const tempResult = taResult.responseBody;
+					return {
+						statusCode: statusCode,
+						responseBody: tempResult.details,
+					};
+				} else if (statusCode === HTTPStatusCode.NOT_FOUND) return taResult;
+					else if (statusCode === HTTPStatusCode.BAD_REQUEST) return taResult;
+					else if (statusCode === HTTPStatusCode.UNAUTHORIZED) {
+					let deletedResponse =
+						UserSessionManagementController.deleteAllSession();
+					if (deletedResponse) window.location.replace(UTSRoutes.LOGINROUTE);
+				}
+			}
+		} catch (error) {
+			return errorDebug(error, 'TaDashboardDAO.getAllPODRevenueDAO');
+		}
+	},
 };
